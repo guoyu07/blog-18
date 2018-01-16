cd7a6214f85c53ab67c99d06f165f3b5f7c674d8
JounQin <admin@1stg.me>
2018-01-16 16:25:47 +0000

    bundle server also

--- src/utils/color.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// tslint:disable-next-line:no-var-requires
-exports.invertColor = require('invert-color');
-//# sourceMappingURL=color.js.map
